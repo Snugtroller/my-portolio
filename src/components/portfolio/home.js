bcf18589d960b597c63f496207510d0c4881934a
Suman S <suman.sh01@gmail.com>
2024-05-06 21:15:17 +0530

    mm

--- src/components/portfolio/home.js
+++ src/components/portfolio/home.js
@@ -1,29 +1,34 @@
 import React from "react";
-import Navbar from "../navbar"; 
+import Navbar from "../navbar";
 import { useNavigate } from 'react-router-dom';
 import "./home.css";
 
+// Define the handleTabClick function outside of the component
+const handleTabClick = (resumeDocumentUrl) => {
+    // Open the resume document URL in a new tab
+    window.open(resumeDocumentUrl, '_blank');
+};
+
 const Home = () => {
-    const navigate = useNavigate(); 
-    const handleTabClick = (path) => {
-        navigate(path); 
-    }
-    const openPDF = () => {
-        const apiUrl = 'http://127.0.0.1:5000/resume';
-    
-        window.open(apiUrl, '_blank');
+    const navigate = useNavigate();
+
+    // Pass the resume document URL as an argument to handleTabClick
+    const handleResumeClick = () => {
+        // Replace 'resume_document_url' with the actual URL of your resume document
+        const resumeDocumentUrl = 'https://drive.google.com/file/d/1qHI9DlupchyfZtkq9pOkExURbHRGg2zN/view?usp=drive_link'; // Example URL
+        handleTabClick(resumeDocumentUrl);
     };
 
     return (
         <div>
-            <Navbar/>
+            <Navbar />
             <div className="main-page">
                 <h2>Hey There,<br />I am Suman</h2>
             </div>
             <div className="wrapper">
-                <button onClick={openPDF} className="resume-button">Resume</button>
+                <button className="resume-button" onClick={handleResumeClick}>Resume</button>
             </div>
-        </div>    
+        </div>
     );
 };
 
